dc243d9395fb88c75777020f2893e5232d3557e6
oozaryanci <oozayranci@gmail.com>
2021-02-22 10:23:51 +0200

    react router is intalled

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCJkMltGia0VMe4xNr5lLWYMU8nlSWGhAQ",
+    authDomain: "slack-clone-2bd90.firebaseapp.com",
+    projectId: "slack-clone-2bd90",
+    storageBucket: "slack-clone-2bd90.appspot.com",
+    messagingSenderId: "477626045840",
+    appId: "1:477626045840:web:c6fc2b0e42330e15af8f15"
+  };
