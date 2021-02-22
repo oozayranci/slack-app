bb8d64bd7f72ed5edc26f0a0c9c46368218099a9
oozaryanci <oozayranci@gmail.com>
2021-02-22 15:35:01 +0200

    router first structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <h1>Lets build slack</h1>
+      <Router>
+        <>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/">
+              <h1>This is the homepage</h1>
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
