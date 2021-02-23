4e8efddf6343933a9a43fc7d4799801bb404aaf5
oozaryanci <ozan.ozayranci@gmail.com>
2021-02-23 13:40:25 +0200

    FiberManualRecordIcon - more styles with styled components

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -45,4 +45,26 @@ const SidebarHeader = styled.div`
     }
 `;
 
-const SidebarInfo = styled.div``;
+const SidebarInfo = styled.div`
+    flex: 1;
+    
+    > h2 {
+        font-size: 15px;
+        font-weight: 900;
+        margin-bottom: 5px;
+    }
+
+    > h3 {
+        display: flex;
+        font-size: 13px;
+        font-weight: 400;
+        align-items: center;
+    }
+
+    > h3 > .MuiSvgIcon-root {
+        font-size: 14px;
+        margin-top: 1px;
+        margin-right: 2px;
+        color: green;
+    }
+`;
