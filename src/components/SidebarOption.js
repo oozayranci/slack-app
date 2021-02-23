b2f0ad6c9caa83c15d6818cd6d0c4290288dea0a
oozaryanci <ozan.ozayranci@gmail.com>
2021-02-23 14:23:34 +0200

    Building the SiderbarOption

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import styled from "styled-components";
 
-function SidebarOption() {
+function SidebarOption({ Icon, title }) {
     return (
         <SidebarOptionContainer>
-            
+            {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
+            {Icon ? (
+            <h3>{title}</h3>
+        ): (
+            <SidebarOptionChannel>
+                <span>#</span> {title}
+            </SidebarOptionChannel>
+        )}
         </SidebarOptionContainer>
+        
     )
 }
 
 export default SidebarOption
 
 const SidebarOptionContainer = styled.div``;
+
+const SidebarOptionChannel = styled.div``;
