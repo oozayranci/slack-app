c48f6e0e2ed262f2a7d76a96f70e5b1fea5f842c
oozaryanci <oozayranci@gmail.com>
2021-02-27 17:02:41 +0200

    styled the chat component's elements

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,7 +6,7 @@ import InfoOutlinedIcon  from '@material-ui/icons/InfoOutlined';
 function Chat () {
   return (
     <ChatContainer>
-      <h1>Chat Screens</h1>
+      <>
       <Header>
         <HeaderLeft>
           <h4><strong>#Room-Name</strong></h4>
@@ -19,15 +19,48 @@ function Chat () {
         </p>
         </HeaderRight>
       </Header>
+
+      <ChatMessages>{/* List out the messages */}</ChatMessages>
+      </>
     </ChatContainer>
   );
 }
 
 export default Chat;
 
-const Header = styled.div``;
-const HeaderLeft = styled.div``;
-const HeaderRight = styled.div``;
+const ChatMessages = styled.div``;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid lightgray;
+`;
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+  > h4 {
+    display: flex;
+    text-transform: lowercase;
+    margin-right: 10px;
+  }
+
+  > h4 > .MuiSvgIcon-root {
+    margin-left: 20px;
+    font-size: 18px;
+  }
+`;
+const HeaderRight = styled.div`
+  > p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+
+  > p > .MuiSvgIcon-root {
+    margin-right: 5px !important;
+    font-size: 16px;
+  }
+`;
 const ChatContainer = styled.div`
     flex: 0.7;
     flex-grow: 1;
