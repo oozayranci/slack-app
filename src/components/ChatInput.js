1a6361ab1306b13b1cce23e110bede77d10f5095
oozaryanci <oozayranci@gmail.com>
2021-03-01 13:18:29 +0200

    ChatInput's elements are created

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import { Button } from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
 
-function ChatInput() {
-    return (
-        <div>
-            
-        </div>
-    )
+function ChatInput({channelName, channelId}) {
+  const sendMessage = e => {
+    e.preventDefault (); // prevents refresh
+  };
+  return (
+    <ChatInputContainer>
+      <form>
+        <input placeholder="Message #ROOM" />
+        <Button hidden type="submit" onClick={sendMessage}>
+          {' '}
+          SEND
+        </Button>
+      </form>
+    </ChatInputContainer>
+  );
 }
 
-export default ChatInput
+export default ChatInput;
+
+const ChatInputContainer = styled.div``;
