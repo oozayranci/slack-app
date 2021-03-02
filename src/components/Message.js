79d4d622467dea7578689d4345254823acb4ad20
oozaryanci <oozayranci@gmail.com>
2021-03-02 07:53:57 +0200

    styled messages styled components

--- src/components/Message.js
+++ src/components/Message.js
@@ -18,5 +18,18 @@ function Message ({message, timestamp, user, userImage}) {
 
 export default Message;
 
-const MessageContainer = styled.div``;
-const MessageInfo = styled.div``;
+const MessageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+`;
+const MessageInfo = styled.div`
+  padding-left: 10px;
+
+  > h4 > span {
+    color: gray;
+    font-weight: 300;
+    margin-left: 4px;
+    font-size: 10px;
+  }
+`;
