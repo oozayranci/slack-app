63fc1542ce49e2ee41ad000103b3c7ff436c7fb8
oozaryanci <oozayranci@gmail.com>
2021-03-02 09:13:52 +0200

    Login component is created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase"
+import Login from './components/Login';
 
 function App () {
   const [user, loading] = useAuthState(auth)
