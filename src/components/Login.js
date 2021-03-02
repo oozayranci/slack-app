197fe4497aa804b906e4d87eb2d97eda5925f409
oozaryanci <oozayranci@gmail.com>
2021-03-02 09:28:00 +0200

    slack logo is added and styled within the proer measures

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from "styled-components";
 
 function Login() {
     return (
-        <div>
-            
-        </div>
+        <LoginContainer>
+            <LoginInnerContainer>
+                <img src="https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png" alt=""/>
+            </LoginInnerContainer>
+        </LoginContainer>
     )
 }
 
-export default Login
+export default Login;
+
+const LoginContainer = styled.div``;
+const LoginInnerContainer = styled.div`
+    > img {
+        object-fit: contain;
+        height: 100px;
+        margin-bottom: 40px;
+    }
+`;
+
